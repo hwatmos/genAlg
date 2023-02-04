e2e76f8efcae103dd7cce36ab008479f820611ba
0xhhh <km.luto@gmail.com>
2023-02-04 09:29:33 -0500

    Start bacterium creator

--- resources/bacteria.js
+++ resources/bacteria.js
@@ -43,7 +43,7 @@
  * 
  * Perform sleep action IFF:
  *     (eCurrent * x1 + eMultiplyLevel * x2)
- *   -----------------------------------------   >  x3
+ *   ----------------------------------------- * 100  >  x3
  *   (2 * eCurrent * x1 * eMultiplyLevel * x2) 
  * 
  * Otherwise, scavenge.
@@ -86,18 +86,18 @@
  *   - eS - energy usage rate per "frame."
  *   - eM - energy amount that is transfered to a child bacterium.
  *   - eR - energy restored per frame while sleeping.
+ * - Bacterium specific properties (not genetic):
+ *   - eCurrent - current level of energy.
  *   - mov1u, mov1d, mov1l, mov1r - which direction the bacterium moved 
  *     1 frame ago (up, down, left, right) - indicators.
  *   - mov2_, mov3_, mov4_, mov5_ - as above but 2, 3, 4, 5 frames ago,
  *     and "_" can be u, d, l, r.
- * - Bacterium specific properties (not genetic):
- *   - eCurrent - current level of energy.
  * - Bacterium specific properties (genetics):
- *   - eMultiplyLevel - energy required to multiply.
- *   - x1 in [0,1]
- *   - x2 in [0,1]
- *   - x3 in [0,1]
- *   - xDNd in [0,1]; D in {U,D,L,R}, N in [1..5], d in {U,D,L,R}
+ *   - eMultiplyLevel in [0..100] - energy required to multiply.
+ *   - x1 in [0,100]
+ *   - x2 in [0,100]
+ *   - x3 in [0,100]
+ *   - xDNd in [0,100]; D in {U,D,L,R}, N in [1..5], d in {U,D,L,R}
  */
 
 //#endregion
@@ -203,15 +203,166 @@ app.stage.addChild(logo);
 
 // #endregion
 /////////////////////////////////////////////////////////////////////////////////
+
 //#region Setup
 /**
  * *Setup
  */
 
 let world = new Array(100).fill(Array(100).fill(0));
+let eM = 50;
+let eS = 1;
+let eR = 2;
 
 // #endregion
 /////////////////////////////////////////////////////////////////////////////////
+
+//#region Bacterium
+
+class Bacterium{
+  constructor(startX, startY, startE, startGenes) {
+    // Find suitable location for the bacterium
+    let successfullyCreated = false;
+    if (world[startY][startX] > -1) {
+      this.X = startX;1
+      this.Y = startY;
+      successfullyCreated = true;
+    }
+    else {
+      for (let i = -1; i <= 1; i++) {
+        for (let j = -1; j <= 1; j++) {
+          if (world[j][i] > -1) {
+            this.X = i;
+            this.Y = j;
+            successfullyCreated = true;
+          }
+        }
+      }
+    }
+    // Check whether location was found
+    if (!successfullyCreated) {
+        //TODO kill bacterium that is at startY,startX
+    }
+    
+    this.eCurrent = startE;
+    this.mov1u = this.mov1d = this.mov1l = this.mov1r = 0;
+    this.mov2u = this.mov2d = this.mov2l = this.mov2r = 0;
+    this.mov3u = this.mov3d = this.mov3l = this.mov3r = 0;
+    this.mov4u = this.mov4d = this.mov4l = this.mov4r = 0;
+    // Genetics:
+    this.genes = startGenes;
+    // Decipher genes:
+    this.x1 = this.genes[0];
+    this.x2 = this.genes[1];
+    this.x3 = this.genes[2]; 
+    this.eMultiplyLevel = this.genes[3];
+    //
+    this.xU1u = this.genes[4];
+    this.xU1r = this.genes[5];
+    this.xU1d = this.genes[6];
+    this.xU1l = this.genes[7];
+    
+    this.xR1u = this.genes[8];
+    this.xR1r = this.genes[9];
+    this.xR1d = this.genes[10];
+    this.xR1l = this.genes[11];
+
+    this.xD1u = this.genes[12];
+    this.xD1r = this.genes[13];
+    this.xD1d = this.genes[14];
+    this.xD1l = this.genes[15];
+
+    this.xL1u = this.genes[16];
+    this.xL1r = this.genes[17];
+    this.xL1d = this.genes[18];
+    this.xL1l = this.genes[19];
+    //
+    this.xU2u = this.genes[20];
+    this.xU2r = this.genes[21];
+    this.xU2d = this.genes[22];
+    this.xU2l = this.genes[23];
+    
+    this.xR2u = this.genes[24];
+    this.xR2r = this.genes[25];
+    this.xR2d = this.genes[26];
+    this.xR2l = this.genes[27];
+
+    this.xD2u = this.genes[28];
+    this.xD2r = this.genes[29];
+    this.xD2d = this.genes[30];
+    this.xD2l = this.genes[31];
+
+    this.xL2u = this.genes[32];
+    this.xL2r = this.genes[33];
+    this.xL2d = this.genes[34];
+    this.xL2l = this.genes[35];
+    //
+    this.xU3u = this.genes[36];
+    this.xU3r = this.genes[37];
+    this.xU3d = this.genes[38];
+    this.xU3l = this.genes[39];
+    
+    this.xR3u = this.genes[40];
+    this.xR3r = this.genes[41];
+    this.xR3d = this.genes[42];
+    this.xR3l = this.genes[43];
+
+    this.xD3u = this.genes[44];
+    this.xD3r = this.genes[45];
+    this.xD3d = this.genes[46];
+    this.xD3l = this.genes[47];
+
+    this.xL3u = this.genes[48];
+    this.xL3r = this.genes[49];
+    this.xL3d = this.genes[50];
+    this.xL3l = this.genes[51];
+    //
+    this.xU4u = this.genes[52];
+    this.xU4r = this.genes[53];
+    this.xU4d = this.genes[54];
+    this.xU4l = this.genes[55];
+    
+    this.xR4u = this.genes[56];
+    this.xR4r = this.genes[57];
+    this.xR4d = this.genes[58];
+    this.xR4l = this.genes[59];
+
+    this.xD4u = this.genes[60];
+    this.xD4r = this.genes[61];
+    this.xD4d = this.genes[62];
+    this.xD4l = this.genes[63];
+
+    this.xL4u = this.genes[64];
+    this.xL4r = this.genes[65];
+    this.xL4d = this.genes[66];
+    this.xL4l = this.genes[67];
+    //
+    this.xU5u = this.genes[68];
+    this.xU5r = this.genes[69];
+    this.xU5d = this.genes[70];
+    this.xU5l = this.genes[71];
+    
+    this.xR5u = this.genes[72];
+    this.xR5r = this.genes[73];
+    this.xR5d = this.genes[74];
+    this.xR5l = this.genes[75];
+
+    this.xD5u = this.genes[76];
+    this.xD5r = this.genes[77];
+    this.xD5d = this.genes[78];
+    this.xD5l = this.genes[79];
+
+    this.xL5u = this.genes[80];
+    this.xL5r = this.genes[81];
+    this.xL5d = this.genes[82];
+    this.xL5l = this.genes[83];
+  }
+
+}
+
+// #endregion
+/////////////////////////////////////////////////////////////////////////////////
+
 //#region Game loop
 
 app.stage.addChild(container);
