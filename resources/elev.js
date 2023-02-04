95fbadf78dd1638064cc7c785117286ed17193dc
0xhhh <km.luto@gmail.com>
2023-02-04 08:11:52 -0500

    Add project desc and logic

--- resources/elev.js
+++ resources/elev.js
@@ -12,11 +12,60 @@
  * TODO orange
  * @param blue
  * 
+ * ! Project description
+ * Simulate simple bacteria.  Bacteria multiply when they have enough energy.
+ * But it takes two bacteria to create a child bacterium (regardles 
+ * of whether that is biologically true in the real world :) ).
+ * They need to eat and rest.  We will code our bacteria with a simple naive
+ * logic that dictates what a bacterium is supposed to do next based on its 
+ * current status.  Current status includes the following properties:
+ * - Energy level (0-100).
+ * - Matter level (0-100).
+ * Bacteria can do one of three actions at any given time:
+ * - Scavenge action.
+ * - Sleep action.
+ * - Multiply
+ * When bacteria scavenges, it moves around the screen searching for food.  
+ * When it sleeps, it processes food turning it into energy.  Energy is req-
+ * uired to multiply and to scavenge.  Multiplying uses up M amount of energy
+ * while scavenging uses up energy at rate S.  Bacteria can multiply only
+ * while they are in scavenge mode.  Two bacteria that are ready to multiply
+ * and that are in scavenge mode will multiply when they come across one 
+ * another (neighboring cells;  each cell has eight neighbors).  
+ * When two bacteria multiply, another bacterium is immediately created 
+ * using genetic algorithm.  The new bacterium received eM energy from 
+ * each of the parent bacteria and but its own starting energy also becomes
+ * eM.
+ * 
+ * * Bacterium's life algorithm
+ * Ready to multiply IFF:
+ *   eCurrent > eMultiplyLevel
+ * 
+ * Perform sleep action IFF:
+ *     (eCurrent * x1 + eMultiplyLevel * x2)
+ *   -----------------------------------------   >  x3
+ *   (2 * eCurrent * x1 * eMultiplyLevel * x2) 
+ * 
+ * Otherwise, scavenge.
+ * 
+ * * Genetics description
+ * Bacterium's genetics involves two 
+ * 
  * ! Code documentation
  * Main functions and variables
  * - Screen canvas:
  *   - maxX, maxY, halfX, halfY
  *   - container (PIXI.Container())
+ * - Bacteria - general properties:
+ *   - eS - energy usage rate per "frame."
+ *   - eM - energy amount that is transfered to a child bacterium.
+ * - Bacterium specific properties (not genetic):
+ *   - eCurrent - current level of energy.
+ * - Bacterium specific properties (genetics):
+ *   - eMultiplyLevel - energy required to multiply.
+ *   - x1 in [0,1]
+ *   - x2 in [0,1]
+ *   - x3 in [0,1]
  */
 
 //#endregion
